081df34226432592cc5fc159e0192a438b0dfe39
osterdalh <32514828+osterdalh@users.noreply.github.com>
2020-11-07 00:29:38 +0100

    finished working good

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import './Table.css'
 
 const Cell = styled.div`
 border: 2px solid white !important;
@@ -9,6 +10,8 @@ background-color: #007bff;
 padding: 0;
 padding-top: 15px;
 font-size: 25px;
+width:100%;
+
 `
 
 const Category = styled.input`
@@ -17,63 +20,174 @@ color: #f4e371;
 text-align: center;
 width: 150px;
 padding: 5px 10px;
-font-size: 20px;
+font-size: 30px;
+width:100%;
+border: 2px solid white !important;
+
+`
+const SelectTeam = styled.div`
+cursor: pointer;
+    opacity:0.6;
+    width:30px;
+    height: 30px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+    border: 1px solid white;
+    transition: border-radius, border, 0.7s;
+    &:hover {
+        opacity:1 !important;
+        border-radius: 0!important;
+        border: 2px solid white;
+    }
 
 `
 
 
 
-const createCol = (title, props) => {
-    return (
-        <div className="row flex-column m-0">
-            <Category defaultValue={title} className="col border category">
-            </Category>
-            <Cell id={`${title}100`} className="col border" onClick={() => props.func(`${title}100`)}>
-            $100
-            {
-                props.func(`${title}100`)
-            }
-            </Cell>
-            <Cell id={`${title}200`} className="col border" onClick={() => props.func(`${title}200`)}>
-                $200
-            </Cell>
-            <Cell id={`${title}300`} className="col border" onClick={() => props.func(`${title}300`)}>
-                $300
-            </Cell>
-            <Cell id={`${title}400`} className="col border" onClick={() => props.func(`${title}400`)}>
-                $400
-             </Cell>
-            <Cell id={`${title}500`} className="col border" onClick={() => props.func(`${title}500`)}>
-                $500
-            </Cell>
-
-        </div>
-    )
-}
 
 
-const Table = (props) => {
+
+class Table extends React.Component {
+    constructor(props) {
+        super()
+
+        this.state = {
+            categories: ["Sport", "Music", "Color", "Film", "Food & Drinks"]
+        }
+    }
 
 
-    return (
-        <div style={{ border: "1px solid white" }} className="Table d-flex justify-content-center  align-items-center">
+    render() {
+        const { teams, handleClick } = this.props
+        const teamIdArray = Object.keys(teams)
+        const categories = this.state.categories;
+        return (
+            <div className="tableContainer">
+                <div className="d-flex justify-content-center m-0 ">
+                    {
+                        categories.map((category, i) => {
+                            return (
+                                <div>
+                                    <Category id={category} defaultValue={category} onChange={(event) => {
+                                        var newCat = categories
+                                        newCat[i] = event.target.value
+                                        this.setState({ categories: newCat })
+                                    }} className=" border category">
+                                    </Category>
 
-            {
+                                    <Cell onMouseOver={() => {
+                                    }}>
+                                        <span id={category + 100}>100$</span>
+                                        <div className=" d-flex justify-content-around pb-2">
+                                            {
 
-                createCol('Film', props)
+                                                teamIdArray.map(teamId => {
+
+                                                    return (
 
 
-            }
+                                                        <SelectTeam  id={teamId + category + 100} onClick={() => handleClick(teamId, category + 100)}  key={teamId} color={teams[teamId].color}>
 
-            {
+                                                        </SelectTeam>
+                                                    )
+                                                })
 
-                createCol('Sport', props)
+                                            }
+                                        </div>
+                                    </Cell>
+                                    <Cell>
+                                        <span id={category + 200}>200$</span>
+                                        <div className=" d-flex justify-content-around pb-2">
+                                            {
+
+                                                teamIdArray.map(teamId => {
+
+                                                    return (
 
 
-            }
-        </div>
+                                                        <SelectTeam id={teamId + category + 200} onClick={() => handleClick(teamId, category + 200)}  key={teamId} color={teams[teamId].color}>
 
-    )
+                                                        </SelectTeam>
+                                                    )
+                                                })
+
+                                            }
+                                        </div>
+                                    </Cell>
+                                    <Cell>
+                                        <span id={category + 300}>300$</span>
+                                        <div className=" d-flex justify-content-around pb-2">
+                                            {
+
+                                                teamIdArray.map(teamId => {
+
+                                                    return (
+
+
+                                                        <SelectTeam id={teamId + category + 300} onClick={() => handleClick(teamId, category + 300)}  key={teamId} color={teams[teamId].color}>
+
+                                                        </SelectTeam>
+                                                    )
+                                                })
+
+                                            }
+                                        </div>
+                                    </Cell>
+                                    <Cell>
+                                        <span id={category + 400}>400$</span>
+                                        <div className=" d-flex justify-content-around pb-2">
+                                            {
+
+                                                teamIdArray.map(teamId => {
+
+                                                    return (
+
+
+                                                        <SelectTeam id={teamId + category + 400} onClick={() => handleClick(teamId, category + 400)}  key={teamId} color={teams[teamId].color}>
+
+                                                        </SelectTeam>
+                                                    )
+                                                })
+
+                                            }
+                                        </div>
+                                    </Cell>
+                                    <Cell>
+                                        <span id={category + 500}>500$</span>
+                                        <div className=" d-flex justify-content-around pb-2">
+                                            {
+
+                                                teamIdArray.map(teamId => {
+
+                                                    return (
+
+
+                                                        <SelectTeam id={teamId + category + 500} onClick={() => handleClick(teamId, category + 500)}  key={teamId} color={teams[teamId].color}>
+
+                                                        </SelectTeam>
+                                                    )
+                                                })
+
+                                            }
+                                        </div>
+                                    </Cell>
+
+                                </div>
+
+                            )
+                        })
+                    }
+
+
+
+                </div>
+            </div>
+
+
+        )
+
+    }
+
 
 }
 
